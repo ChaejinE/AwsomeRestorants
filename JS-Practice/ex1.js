60bd0e99d1822323e280e7ea8d69333afd918798
ChaejinE <joung6517@gmail.com>
2022-04-09 21:43:26 +0900

    변수 선언

---
+++ JS-Practice/ex1.js
@@ -0,0 +1,29 @@
+var num = 1;
+
+console.log(num);
+
+/* 
+재할당 : 이미 선언된 변수에 새로운 값 할당
+재선언 : 이미 선언된 변수를 다시 선언
+var는 재할당, 재선언이 가능
+let, const는 재선언이 불가능
+- let은 재할당 가능, const는 재할당 불가능
+*/
+let num2 = 2;
+const num3 = 3;
+
+
+/*
+문자열 자료형
+*/
+
+let string = "안녕"
+
+/*
+논리형 자료형
+*/
+
+let bool = true;
+let t = 100 > 10;
+
+console.log(t);
